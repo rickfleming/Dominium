c05335b50216452ba646a4f5976633bd34461c67
Richard Fleming <rfleming@acqusys.com>
2019-08-01 06:45:01 -0500

    Added EntityFramework Store

---
+++ Dominium.Store.EntityFramework/EntityFrameworkStore.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dominium.Store.EntityFramework
+{
+	public class EntityFrameworkStore : IStore
+	{
+		private readonly DbContext _ctx;
+
+		public EntityFrameworkStore(DbContext ctx) => _ctx = ctx;
+		
+		public async Task<TRoot> Load<TRoot>(params object[] keyValues) where TRoot : class
+			=> await _ctx.FindAsync<TRoot>(keyValues);
+
+		public async Task Save<TRoot>(TRoot root) where TRoot : class
+			=> await _ctx.SaveChangesAsync();
+	}
+}
